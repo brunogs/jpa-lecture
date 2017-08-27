47e0511f58c4de4354534294f41a95caf00ecdd4
Weliff Lima <weliff.14@gmail.com>
2017-08-27 01:55:30 -0300

    test

--- src/main/java/br/com/belezanaweb/jpa/jpalecture/JpaLectureApplication.java
+++ src/main/java/br/com/belezanaweb/jpa/jpalecture/JpaLectureApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpaLectureApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(JpaLectureApplication.class, args);
 	}
