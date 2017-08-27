8cfe71ce24ce4d616b677cbef2daf78a81450ab2
Weliff Lima <weliff.14@gmail.com>
2017-08-27 17:23:58 -0300

    fixes

--- src/test/java/br/com/belezanaweb/jpa/jpalecture/JpaLectureApplicationTests.java
+++ src/test/java/br/com/belezanaweb/jpa/jpalecture/JpaLectureApplicationTests.java
@@ -1,11 +1,40 @@
 package br.com.belezanaweb.jpa.jpalecture;
 
+import br.com.belezanaweb.jpa.jpalecture.domain.Customer;
+import br.com.belezanaweb.jpa.jpalecture.domain.Order;
+import br.com.belezanaweb.jpa.jpalecture.repository.CustomerRepository;
+import br.com.belezanaweb.jpa.jpalecture.repository.OrderRepository;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+import static java.util.stream.IntStream.range;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class JpaLectureApplicationTests {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        Random random = new Random();
+        range(0, 50).forEach(customerIndex -> {
+            int randomAge = random.nextInt(50) + 10;
+            Customer customer = customerRepository.save(new Customer("customer" + customerIndex, randomAge));
+            range(1, random.nextInt(50) + 1)
+                    .forEach(orderIndex -> orderRepository.save(new Order(customer, BigDecimal.valueOf(orderIndex))));
+        });
+    }
 
 }
--- src/test/java/br/com/belezanaweb/jpa/jpalecture/nativequeries/NativeQueries.java
+++ src/test/java/br/com/belezanaweb/jpa/jpalecture/nativequeries/NativeQueries.java
@@ -3,15 +3,12 @@ package br.com.belezanaweb.jpa.jpalecture.nativequeries;
 import br.com.belezanaweb.jpa.jpalecture.JpaLectureApplicationTests;
 import br.com.belezanaweb.jpa.jpalecture.domain.Customer;
 import br.com.belezanaweb.jpa.jpalecture.domain.Order;
-import br.com.belezanaweb.jpa.jpalecture.dto.OrderDTO;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class NativeQueries extends JpaLectureApplicationTests {
 
@@ -19,9 +16,16 @@ public class NativeQueries extends JpaLectureApplicationTests {
     private EntityManager entityManager;
 
     @Test
-    public void nativeQuery() throws Exception {
-        Query nativeQuery = entityManager.createNativeQuery("select c.id, c.name, c.age from customers c", Customer.class);
+    public void nativeQueryBasic() throws Exception {
+        Query nativeQuery = entityManager.createNativeQuery("select *  from customers c", Customer.class);
         List<Customer> customers = nativeQuery.getResultList();
+        System.out.println(customers);
+    }
+
+    @Test
+    public void nativeQueryProjectionObject() throws Exception {
+        Query nativeQuery = entityManager.createNativeQuery("select  * from orders c", Order.class);
+        List<Order> customers = nativeQuery.getResultList();
         System.out.println(customers);
     }
 }
